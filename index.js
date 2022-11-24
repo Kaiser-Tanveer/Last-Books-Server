e1f7d86021a3260127941ad8fe89988023c89fda
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-11-25 01:40:52 +0600

    products added in the categories

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
+const { query } = require('express');
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        // creating Books category Collection 
+        const categoriesCollection = client.db('lastBooks').collection('categories');
+
+        // getting data from categories categoriesCollection
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        })
+
+        // getting data by id 
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: ObjectId(id) }
+            const products = await categoriesCollection.findOne(filter);
+            res.send(products);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+run()
+    .catch(err => console.log(err));
+
+
+
+app.get('/', (req, res) => {
+    res.send('Books server is running..');
+});
+
+app.listen(port, (req, res) => {
+    console.log(`server is running on port ${port}`);
+});
